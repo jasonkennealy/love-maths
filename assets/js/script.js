5159a34fe593a0d2b042152aa8b280ec0b5e446e
jasonkennealy <>
2023-08-21 14:41:57 +0000

    add javescript for submit button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+let buttons = document.getElementsByTagName("button");
+
+for (button of buttons) {
+button.addEventListener("click", function() {
+    if(this.getAttribute("data-type") === "submit" ) {
+        alert("You've clicked submit!");
+    } else {
+        let.gameType = this.getAttribute("data-type");
+        alert(`You clicked ${gameType}`);
+    }
+})
+}
+})
+
+
 function runGame() {
 
 }
